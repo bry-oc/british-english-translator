45a0cdf4793fd0fbf126b7e6f76307608fa4e451
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-06-29 01:15:03 -0400

    proper period handling

--- components/translator.js
+++ components/translator.js
@@ -10,38 +10,48 @@ class Translator {
 
     translateToBritish(text) {
         console.log(text);
-        let words = text.split(/\s|(?=[\.](?!\s))/);
-        //let words = text.split(/\s|(?=[\.])/i);
+        //let words = text.split(/\s|(?=[\.](?!\s))/);
+        let words = text.split(/\s|(?=[\.])/);
         console.log(words);
         let translated = []
+        let word;
         for (let i = 0; i < words.length; i++) {
             if (americanOnly[words[i].toLowerCase()]) {
-                translated.push('<span class="highlight">' + americanOnly[words[i].toLowerCase()] + '</span>');
+                word = '<span class="highlight">' + americanOnly[words[i].toLowerCase()] + '</span>';
             } else if(i + 1 <= words.length - 1 && americanOnly[words[i].toLowerCase() + " " + words[i+1].toLowerCase()]){
-                translated.push('<span class="highlight">' + americanOnly[words[i].toLowerCase() + " " + words[i+1].toLowerCase()] + '</span>');
+                word = '<span class="highlight">' + americanOnly[words[i].toLowerCase() + " " + words[i+1].toLowerCase()] + '</span>';
                 i++;
             } else if(i + 2 <= words.length - 1 && americanOnly[words[i].toLowerCase() + " " + words[i+1].toLowerCase() + " " + words[i+2].toLowerCase()]){
-                translated.push('<span class="highlight">' + americanOnly[words[i].toLowerCase() + " " + words[i+1].toLowerCase() + " " + words[i+2].toLowerCase()] + '</span>');
+                word = '<span class="highlight">' + americanOnly[words[i].toLowerCase() + " " + words[i+1].toLowerCase() + " " + words[i+2].toLowerCase()] + '</span>';
                 i++;
                 i++;
             } else if (americanToBritishSpelling[words[i].toLowerCase()]) {
-                translated.push('<span class="highlight">' + americanToBritishSpelling[words[i].toLowerCase()] + '</span>');
-            } else if (americanToBritishTitles[words[i].toLowerCase()]) {
+                word = '<span class="highlight">' + americanToBritishSpelling[words[i].toLowerCase()] + '</span>';
+            } else if (americanToBritishTitles[words[i].toLowerCase() + '.']) {
+                console.log('this is a title');
                 let title = [];
-                let titleLength = americanToBritishTitles[words[i].toLowerCase()].length;
+                let titleLength = americanToBritishTitles[words[i].toLowerCase() + '.'].length;
                 for(let j = 0; j < titleLength; j++){
                     if(j === 0) {
-                        title.push(americanToBritishTitles[words[i].toLowerCase()][j].toUpperCase());
+                        title.push(americanToBritishTitles[words[i].toLowerCase() + '.'][j].toUpperCase());
                     } else {
-                        title.push(americanToBritishTitles[words[i].toLowerCase()][j]);
+                        title.push(americanToBritishTitles[words[i].toLowerCase() + '.'][j]);
                     }                    
                 }
                 title = title.join('');          
-                translated.push('<span class="highlight">' + title + '</span>');
+                word = '<span class="highlight">' + title + '</span>';
+                i++;
             } else if(words[i].match(/\d\d?:\d{2}/)){
-                translated.push('<span class="highlight">' + words[i].replace(':','.') + '</span>');
+                word = '<span class="highlight">' + words[i].replace(':','.') + '</span>';
             } else {
-                translated.push(words[i]);
+                word = words[i];
+            }
+            if(words[i + 1] === '.'){
+                words[i] += '.';
+                translated.push(word + '.');
+                i++;
+            } else {
+                translated.push(word);
             }
         }
         if( translated[translated.length - 1] === '.'){
